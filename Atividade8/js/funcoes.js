ce4263ab78a90cb227bce7158edcaa3030c9c6e5
marques-gabriell <magicomnstr@gmail.com>
2026-06-24 00:50:08 +0000

    Criação e atualização do JS atividade 8

---
+++ Atividade8/js/funcoes.js
@@ -0,0 +1,58 @@
+function soma() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) + Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    subtraçao("subtração",primeiroNumero, segundoNumero, resultado)
+
+    salvarHistorico("Soma",primeiroNumero, segundoNumero, resultado)
+}
+
+function subtraçao() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) - Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("Subtraçao",primeiroNumero, segundoNumero, resultado)
+}
+
+function multiplicação() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) * Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("Multiplicação",primeiroNumero, segundoNumero, resultado)
+}
+
+function divisao() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) / Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("Divisao",primeiroNumero, segundoNumero, resultado)
+}
+
+function salvarHistorico(nomeDaFuncao, num1, num2, resultado) {
+    console.log("Função: " + nomeDaFuncao);
+    console.log("Primeiro numero: " + num1 + ", SegundoNumero: " + num2);
+    console.log("Resultado: " + resultado);
+    console.log("--------------------------------------");
+
+
+    let textoJSON = JSON.stringify(objetoNome)
+    localStorage.setItem("dadoNome", textoJSON)
+
+    document.getElementById("soma").innerText = resultado;
+}
